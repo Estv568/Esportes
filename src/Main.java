910e1e2ea59984517926bb2467882bba927e2e46
estevao domingues <eltororo322@gmail.com>
2023-11-09 08:23:31 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Pessoa qualquer = new Pessoa();
+
+        qualquer.setNome("Rebeca Andrade");
+        System.out.println(qualquer.getNome());
+
+
+    Esportista jogadora =  new Esportista();
+    Esportista surfista = new Esportista();
+
+    jogadora.setNome("Taisa do volei");
+    System.out.println(jogadora.getNome());
+
+    //polimorfismo
+        Pessoa outra = new Pessoa();
+        String retorno = outra.sacar();
+        System.out.println(retorno);
+        outra = new Esportista();
+        retorno = outra.sacar();
+        System.out.println(retorno);
+}
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,59 @@
+import java.time.LocalDate;
+
+public class Pessoa {
+
+    private String nome;
+    private String cpf;
+    private LocalDate dataDeNascimento;
+    private double peso;
+    private double altura;
+
+    //getters and setters
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    //metodo
+    public String sacar(){
+        return "Não sei sacar!";
+    }
+
+}
